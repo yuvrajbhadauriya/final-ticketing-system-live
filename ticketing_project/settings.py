6b0953175a421425b9f9578466eeaed876cd3465
Yuvraj Bhadauriya <yuvi673758@gmail.com>
2025-08-17 13:07:25 +0530

    Update settings.py

--- ticketing_project/settings.py
+++ ticketing_project/settings.py
@@ -39,8 +39,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yuvraj.tedx@gmail.com'  # <-- REPLACE THIS with your full Gmail address
-EMAIL_HOST_PASSWORD = 'xiiohxweueoggohy' # This is your App Password (spaces removed)
+EMAIL_HOST_USER = 'tedxvips.tickets@gmail.com'  # <-- REPLACE THIS with your full Gmail address
+EMAIL_HOST_PASSWORD = 'rimukobgqvntqzfl' # This is your App Password (spaces removed)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STATIC_URL = 'static/'
