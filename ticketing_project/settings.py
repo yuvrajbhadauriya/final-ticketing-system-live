dd8ec090d9d701b7fd40f23886638bb4817f6aef
Yuvraj Bhadauriya <yuvi673758@gmail.com>
2025-08-17 13:12:30 +0530

    Update settings.py

--- ticketing_project/settings.py
+++ ticketing_project/settings.py
@@ -14,7 +14,7 @@ CSRF_TRUSTED_ORIGINS = ['https://yuvrajbhadauriya.pythonanywhere.com']
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-    'anymail', 'tickets',
+    'import_export','anymail', 'tickets',
 ]
 
 MIDDLEWARE = [
