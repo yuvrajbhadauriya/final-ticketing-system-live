7d9ecf29ee977803d6b0c6bfbcf2f7991c94ffd4
yuvrajbhadauriya <yuvi673758@gmail.com>
2025-08-18 22:40:03 +0530

    final domain update

--- ticketing_project/settings.py
+++ ticketing_project/settings.py
@@ -8,8 +8,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'a-default-secret-key-for-local
 
 # Production settings
 DEBUG = True
-ALLOWED_HOSTS = ['yuvrajbhadauriya.pythonanywhere.com', 'tedxvips2025tickets.pythonanywhere.com', "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ['https://yuvrajbhadauriya.pythonanywhere.com', 'https://tedxvips2025tickets.pythonanywhere.com']
+ALLOWED_HOSTS = ['yuvrajbhadauriya.pythonanywhere.com', 'tedxvips2025tickets.pythonanywhere.com', "127.0.0.1", "passes.tedxvips.com", "www.passes.tedxvips.com"]
+CSRF_TRUSTED_ORIGINS = ['https://yuvrajbhadauriya.pythonanywhere.com', 'https://tedxvips2025tickets.pythonanywhere.com', 'https://passes.tedxvips.com', 'https://www.passes.tedxvips.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
